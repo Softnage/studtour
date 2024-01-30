8ccebde2bb628fe32e35c78024b03735ba2e4710
Kenworld <stunnerweezy@gmail.com>
2024-01-30 10:38:21 +0000

    New Update 1 jan 2024

---
+++ app.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+    window.addEventListener("scroll", function () {
+        var nav = this.document.querySelector("nav");
+        nav.classList.toggle("sticky", this.window.scrollY > 0)
+    })
+
+    document.addEventListener("DOMContentLoaded", function () {
+        // Get the current year
+        var currentYear = new Date().getFullYear();
+
+        // Update the copyright text
+        var copyrightElement = document.getElementById("copyright");
+        copyrightElement.innerHTML = '© Studstay ' + currentYear + '. All rights reserved.';
+    });
+
+    // Configure/customize these variables.
+    var showChar = 250;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "...Read More";
+    var lesstext = "Read Less";
+
+
+    $('.more-content').each(function () {
+        var content = $(this).html();
+
+        if (content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function () {
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
+///slid
+var slideIndex = 0;
+showSlides();
+var slides, dots;
+
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 8 seconds
+}
+
+function plusSlides(position) {
+    slideIndex += position;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    else if (slideIndex < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function currentSlide(index) {
+    if (index > slides.length) { index = 1 }
+    else if (index < 1) { index = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index - 1].style.display = "block";
+    dots[index - 1].className += " active";
+}
+//hamburger menu
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+    const menuIcon = document.querySelector('.menu-icon');
+    menuIcon.classList.toggle('active');
+}
